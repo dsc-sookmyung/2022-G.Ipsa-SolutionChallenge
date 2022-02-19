6c7938b9e1f63c1b6e88a152403a541e00a3139e
JYBESiU <jychoi.besiu@gmail.com>
2022-02-19 20:36:25 +0900

    src 구조, convention

--- frontend/GIpsa/src/App.tsx
+++ frontend/GIpsa/src/App.tsx
@@ -27,28 +27,38 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<{
+import S from './Styles';
+
+interface a {
   title: string;
-}> = ({children, title}) => {
+}
+
+const Section: React.FC<React.PropsWithChildren<a>> = ({
+  children,
+  title,
+}: React.PropsWithChildren<a>) => {
   const isDarkMode = useColorScheme() === 'dark';
+
   return (
-    <View style={styles.sectionContainer}>
+    <View style={S.sectionContainer}>
       <Text
         style={[
-          styles.sectionTitle,
+          S.sectionTitle,
           {
             color: isDarkMode ? Colors.white : Colors.black,
           },
-        ]}>
+        ]}
+      >
         {title}
       </Text>
       <Text
         style={[
-          styles.sectionDescription,
+          S.sectionDescription,
           {
             color: isDarkMode ? Colors.light : Colors.dark,
           },
-        ]}>
+        ]}
+      >
         {children}
       </Text>
     </View>
@@ -67,49 +77,18 @@ const App = () => {
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+        style={backgroundStyle}
+      >
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          }}
+        >
+          <Section title="Step One">Editddasdasdddasdasdasdsa</Section>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
