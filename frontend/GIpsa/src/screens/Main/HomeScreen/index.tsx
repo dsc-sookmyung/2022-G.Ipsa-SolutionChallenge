e2f9fa0fde7d5669803cbe22feb07724564722a5
Jongyoon Choi <jychoi.besiu@gmail.com>
2022-03-10 00:08:04 +0900

    최신 업로드 / 인기 스토리 구분 (#19)

--- frontend/GIpsa/src/screens/Main/HomeScreen/index.tsx
+++ frontend/GIpsa/src/screens/Main/HomeScreen/index.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 import S from './Styles';
 import { MainTabScreenProps } from 'navigator/types';
 import { StoryTopic } from 'shared/components';
-import { Story } from 'shared/types';
 import { useStories } from 'shared/hook/useStories';
+import moment from 'moment';
 
 export type HomeScreenParams = {
   a: number;
@@ -14,10 +14,19 @@ export type HomeScreenParams = {
 const HomeScreen = ({ navigation, route }: MainTabScreenProps<'Home'>) => {
   const { stories, loading, mutate } = useStories();
 
+  const timeSortedStories = stories?.sort(
+    (a, b) =>
+      moment(b.createdAt).toDate().getTime() -
+      moment(a.createdAt).toDate().getTime()
+  );
+
+  const likeSortedStories = stories?.sort((a, b) => b.likes - a.likes);
+
   return (
     <ScrollView style={S.container}>
       <Text style={S.title}>HomeScreen</Text>
-      <StoryTopic title="인기 스토리" stories={stories} />
+      <StoryTopic title="최신 업로드 스토리" stories={timeSortedStories} />
+      <StoryTopic title="인기 스토리" stories={likeSortedStories} />
     </ScrollView>
   );
 };
