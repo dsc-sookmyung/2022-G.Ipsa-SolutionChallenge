b05b38ff6597375edf5752ea91e8b37cdd046396
NayeonKeum <rmaskdus0208@gmail.com>
2022-03-28 02:47:43 +0900

    파일정리 22

---
+++ frontend/GIpsa/src/shared/components/TrackPlayer/TrackPlayer.ts
@@ -0,0 +1,16 @@
+import TrackPlayer from 'react-native-track-player';
+import { Story } from 'shared/types';
+
+const start = (story: Story) => {
+  TrackPlayer.setupPlayer();
+  TrackPlayer.add({
+    id: story.id,
+    url: story.audioFileSrc,
+    title: story.title,
+    artist: '' + story.creatorId,
+    artwork: story.thumbnailImageSrc,
+    duration: story.duration,
+  });
+  TrackPlayer.play();
+};
+export default start;
